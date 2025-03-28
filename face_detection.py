cedf1c00c403faba5e4ae7b456b488ede08b96c8
EricMuzzo <128553196+EricMuzzo@users.noreply.github.com>
2025-03-28 18:31:17 -0400

    Changed URL I/O

--- face_detection.py
+++ face_detection.py
@@ -23,8 +23,8 @@ def detect_faces(input_frame):
             
     return input_frame
 
-rtmp_input_url = "rtmp://127.0.0.1/live/pi_0001"  # Where the Raspberry Pi streams unprocessed video
-rtmp_output_url = "rtmp://127.0.0.1/play/pi_0001"   # Where the processed video will be published
+rtmp_input_url = "rtmp://queueview.ca/rawfeed/pi_0001"  # Where the Raspberry Pi streams unprocessed video
+rtmp_output_url = "rtmp://queueview.ca/processed/pi_0001"   # Where the processed video will be published
 
 stream = cv2.VideoCapture(rtmp_input_url)
 
